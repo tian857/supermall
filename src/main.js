8141eeda5c9ff06a32571748af447dcc0e7ba231
tian857 <1456294170@qq.com>
2021-04-24 20:15:01 +0800

    第二次初始化项目

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
-import store from './store'
+import router from './router'
 
-createApp(App).use(store).mount('#app')
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+  router
+}).$mount('#app')
